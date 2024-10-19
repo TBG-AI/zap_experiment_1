e9cd685c76764accbe8bdf6e79313f3429441847
jlee <vxbrandon@gmail.com>
2024-10-18 17:37:56 -0700

    initial commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,27 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+// import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { lusitana } from '@/app/ui/fonts';
+
+// export default function AcmeLogo() {
+//   return (
+//     <div
+//       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+//     >
+//       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+//       <p className="text-[44px]">ZAP</p>
+//     </div>
+//   );
+// }
+
+import { IoFootball } from "react-icons/io5";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <IoFootball className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">ZAP</p>
     </div>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+/* Next.js automatically optimizes fonts in the application when you use 
+the next/font module. */
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ['latin'] });
