4605dbe9da78d77b5a5cd30c71e5108737ad1155
hunterZh37 <71850140+hunterZh37@users.noreply.github.com>
2024-10-19 19:59:09 -0700

    ✅ Successfully add NextUI

---
+++ app/dashboard/components/teamName.jsx
@@ -0,0 +1,13 @@
+import teamLogos from '../../utils/teamLogos'
+
+
+export function teamName({ homeTeam, awayTeam, time }) {
+  const homeTeamLogo = teamLogos[homeTeam];
+  const awayTeamLogo = teamLogos[awayTeam];
+  console.log({homeTeamLogo})
+  return (
+    <div>
+     
+    </div>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link';
 import { Form } from './components/form';
-import {LoginButton} from './components/loginButton'
-
+import {Button} from "@nextui-org/button";
+import {NextUIProvider} from "@nextui-org/react";
 
 export default function Login() {
   return (
+    <NextUIProvider>
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           <h3 className="text-xl font-semibold">Sign In to ZAP</h3>
         </div>
         <Form>
-         <LoginButton> Log in </LoginButton>
+        <Button variant="bordered">
+        Log In
+       </Button>  
         </Form>
       </div>
+   
     </div>
+    </NextUIProvider>
   );
 }
