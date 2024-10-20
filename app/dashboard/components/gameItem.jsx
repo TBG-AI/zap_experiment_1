9d0fa1c05d12c45ee96b22f5cd88a49ad2acd7e6
hunterZh37 <71850140+hunterZh37@users.noreply.github.com>
2024-10-19 23:12:51 -0700

    🔧 Flexbox bug fixing the aligning of the icons

--- app/dashboard/components/gameItem.jsx
+++ app/dashboard/components/gameItem.jsx
@@ -20,24 +20,26 @@ export function GameItem({ homeTeam, awayTeam, time, onGameSelect}) {
   return (
     <Card isHoverable isPressable allowTextSelectionOnPress className="w-[500px]" onPress={handlePress}>
       <CardBody className="flex">
-        <div className="flex justify-center items-center">
-        <div className = "flex justify-between items-center">
+        <div className="flex justify-center items-center ">
+        <div className = "flex flex-none justify-between items-center w-20">
         <p className = "text-lg">{homeTeam}</p>
+        </div>
+
+        <div className = "flex flex-none justify-center items-center w-60">
         <Image
           alt="Home Team Logo"
           src={homeTeamLogo}
           className="m-2"
         />
-        </div>
-
         <p className = "text-[#a6a6a6] text-sm items-center">{time}</p>
-
-       <div className = "flex justify-between items-center">
         <Image
           alt="Away Team Logo"
           src={awayTeamLogo}
           className="m-2"
         />
+        </div>
+
+        <div className = "flex flex-none justify-between items-center w-20">
         <p className = "text-lg">{awayTeam}</p>
         </div>
         </div>
